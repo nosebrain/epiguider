d3ca7c7f6bdbf1992740a8796f4b7b9b82cfe87d
Daniel Zoller <nosebrain@gmx.net>
2017-07-23 17:53:47 +0200

    fixed ajax adding of shows

--- src/main/resources/de/nosebrain/epiguider/schema/epiguider-schema.sql
+++ src/main/resources/de/nosebrain/epiguider/schema/epiguider-schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `series` (
   `name` varchar(255) NOT NULL DEFAULT '',
-  `parser_id` varchar(255) DEFAULT NULL,
-  `series_id` varchar(255) DEFAULT NULL,
+  `parser_id` varchar(255) NOT NULL,
+  `series_id` varchar(255) NOT NULL,
   PRIMARY KEY (`name`,`parser_id`,`series_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
